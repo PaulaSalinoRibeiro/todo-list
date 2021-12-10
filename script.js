700fe6550e78232a0b6ef60e465d3563e8ec7170
PAULA RIBEIRO <paulasalinor@gmail.com>
2021-12-10 15:25:11 -0300

    adiciona via js um paragrafo

---
+++ script.js
@@ -0,0 +1,5 @@
+let paragraph = document.createElement('p');
+let header = document.getElementById('header');
+header.appendChild(paragraph);
+paragraph.id = 'funcionamento';
+paragraph.innerText = 'Clique duas vezes em um item para marcá-lo como completo';
