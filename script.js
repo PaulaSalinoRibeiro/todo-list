aab63e1b1706aef7ce90e4dfcfef0d0680191a0c
PAULA RIBEIRO <paulasalinor@gmail.com>
2021-12-10 16:08:00 -0300

    cria evento que adiciona texto em uma lista

--- script.js
+++ script.js
@@ -9,6 +9,21 @@ header.appendChild(input);
 input.type = 'text';
 input.id = 'texto-tarefa';
 
+
 let orderList = document.createElement('ol');
 header.appendChild(orderList);
 orderList.id = 'lista-tarefas';
+
+let button = document.createElement('button');
+header.appendChild(button);
+button.id = 'criar-tarefas';
+button.innerText = 'Adicionar';
+
+button.addEventListener('click', addItem);
+
+function addItem(){
+    let button = document.getElementById('texto-tarefa').value; // https://stackoverflow.com/questions/11563638/how-do-i-get-the-value-of-text-input-field-using-javascript
+    let item = document.createElement('li');
+    orderList.appendChild(item);
+    orderList.innerText = button;
+}
